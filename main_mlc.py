5a10c861dda85d94ba01ec6ad4119eef67a9f441
David Andersson <david.andersson@hm.com>
2021-11-19 16:11:24 +0100

    changed key and value in save_state dict to be backbone instead of missing arg

--- main_mlc.py
+++ main_mlc.py
@@ -398,7 +398,7 @@ def main_worker(args, logger):
             if dist.get_rank() == 0:
                 save_checkpoint({
                     'epoch': epoch + 1,
-                    'arch': args.arch,
+                    'backbone': args.backbone,
                     'state_dict': state_dict,
                     'best_mAP': best_mAP,
                     'optimizer' : optimizer.state_dict(),
@@ -408,7 +408,7 @@ def main_worker(args, logger):
             if math.isnan(loss) or math.isnan(loss_ema):
                 save_checkpoint({
                     'epoch': epoch + 1,
-                    'arch': args.arch,
+                    'backbone': args.backbone,
                     'state_dict': model.state_dict(),
                     'best_mAP': best_mAP,
                     'optimizer' : optimizer.state_dict(),
