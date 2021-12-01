50e57c42e4e6fd3a1e9ef9b407409752df1a07ea
David Andersson <david.andersson@hm.com>
2021-12-01 10:06:40 +0100

    changed to read from blob storage

--- lib/dataset/pimdataset.py
+++ lib/dataset/pimdataset.py
@@ -31,7 +31,7 @@ class AttributeDataset(data.Dataset):
         multilabel: bool = False,
         n_classes: Optional[int] = None,
         transform: Optional[transforms.Compose] = None,
-        root_path: str = ".",
+        root_path: str = "/dbfs/mnt/pim",
     ):
         self.castors = castors
         self.labels = labels
